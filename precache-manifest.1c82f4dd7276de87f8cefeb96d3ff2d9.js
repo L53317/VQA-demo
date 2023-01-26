176e572070c32c24edbd5a2b27e689bf8717340c
liu <liuxh.0@qq.com>
2023-01-27 00:59:36 +0100

    Updates

---
+++ precache-manifest.1c82f4dd7276de87f8cefeb96d3ff2d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca39a8173468b70f468b59e839ec9579",
+    "url": "/easy-VQA-app/index.html"
+  },
+  {
+    "revision": "235ae85c0344c9f40e3a",
+    "url": "/easy-VQA-app/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ec57ba8278e948173fce",
+    "url": "/easy-VQA-app/static/css/main.7adf00f7.chunk.css"
+  },
+  {
+    "revision": "235ae85c0344c9f40e3a",
+    "url": "/easy-VQA-app/static/js/2.e8f5d031.chunk.js"
+  },
+  {
+    "revision": "ec57ba8278e948173fce",
+    "url": "/easy-VQA-app/static/js/main.a72a3910.chunk.js"
+  },
+  {
+    "revision": "75c65e4faf9d02ce61b8",
+    "url": "/easy-VQA-app/static/js/runtime-main.a1979f71.js"
+  }
+]);
